82374bb7f018a14b202a4bdcdceea77bcbe2c390
詹 家彰 <kz85430@gmail.com>
2019-12-30 17:35:30 +0800

    snippet

--- test.py
+++ test.py
@@ -20,3 +20,43 @@ print(ar.shape)
 
 ar=np.array([range(24)]).T
 print(ar.shape)
+
+
+
+'''
+{
+	// Place your snippets for python here. Each snippet is defined under a snippet name and has a prefix, body and 
+	// description. The prefix is what is used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
+	// $1, $2 for tab stops, $0 for the final cursor position, and ${1:label}, ${2:another} for placeholders. Placeholders with the 
+	// same ids are connected.
+	// Example:
+	// "Print to console": {
+	// 	"prefix": "log",
+	// 	"body": [
+	// 		"console.log('$1');",
+	// 		"$2"
+	// 	],
+	// 	"description": "Log output to console"
+	// }
+	"folding code": {
+	"prefix": "rrr",
+	 	"body": "#region $1",
+		 "description": "region"
+	},
+	"folding code2":{
+		"prefix": "eee",
+			 "body": "#endregion $1",
+			 "description": "endregion"
+		},
+	"print1":{
+		"prefix": "ppp",
+		"body": "print($1)",
+		"description": "print1"
+		},
+	"type":{
+		"prefix": "ptt",
+		"body": "print(type($1))",
+		"description": "type1"
+		},
+}
+'''
